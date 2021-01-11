b19926f2e391a74925e7404bd5ea1930c9e8e081
apolzek <viniciusgomes955@gmail.com>
2021-01-11 08:31:28 -0300

    callable - python3

--- functions/html_generator_v4.py
+++ functions/html_generator_v4.py
@@ -1,4 +1,15 @@
-def tag_bloco(conteudo, *args, classe='success', inline=False):
+def tag_block(content, *args, select_class='success', inline=False):
     tag = 'span' if inline else 'div'
-    html = conteudo if not callable(conteudo) else conteudo(*args)
-    return f'<{tag} class="{classe}">{html}</{tag}>'
+    html = content if not callable(content) else content(*args)
+    return f'<{tag} class="{select_class}">{html}</{tag}>'
+
+
+def tag_list(*itens):
+    list = ''.join(f'<li>{item}</li>' for item in itens)
+    return f'<ul> {list} </ul>'
+
+
+if __name__ == '__main__':
+    print(tag_block(tag_list('Item1', 'Item2'), select_class='info'))
+    # mandatory to use named parameters
+    print(tag_block(tag_list('x', 'y'), select_class='info', inline=True))
