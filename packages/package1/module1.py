26dfb2f011324e1259b341c41a2b925060cbcc25
apolzek <viniciusgomes955@gmail.com>
2021-01-12 22:38:40 -0300

    Init packages session

---
+++ packages/package1/module1.py
@@ -0,0 +1,6 @@
+print('imported')
+print(__name__)
+print(__package__)
+
+def sum(x, y):
+    return x + y
---
+++ packages/package_v1.py
@@ -0,0 +1,5 @@
+from package1 import module1
+
+print(type(module1))
+
+print(module1.sum(1, 10))
